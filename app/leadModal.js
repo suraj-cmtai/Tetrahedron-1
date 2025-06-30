be30dbef8bac07342cb6a12c2b8c04893cbb4c77
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-06-30 18:04:40 +0530

    fix contact modal

--- app/leadModal.js
+++ app/leadModal.js
@@ -9,10 +9,27 @@ export default function Modal({ isOpen, onClose }) {
                 ref={node => {
                     if (node) {
                         node.style.setProperty("display", "flex", "important");
+                        node.style.setProperty("position", "relative", "important");
                     }
                 }}
             >
-                <button className="custom-modal-close" onClick={onClose}>&times;</button>
+                <button 
+                    className="custom-modal-close" 
+                    onClick={onClose}
+                    style={{
+                        position: "absolute",
+                        top: "16px",
+                        right: "16px",
+                        zIndex: 1000,
+                        fontSize: "24px",
+                        background: "transparent",
+                        border: "none",
+                        cursor: "pointer",
+                        color: "#333"
+                    }}
+                >
+                    &times;
+                </button>
                 
                 <ContactForm />
             </div>
