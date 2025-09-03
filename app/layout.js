a5b89355a58282ab65d0b68de4367d441306a75a
Vishal Sharma <vishal.sharma@completrix.com>
2025-09-03 11:37:13 +0530

    update the root layout and thankyou page for GTM

--- app/layout.js
+++ app/layout.js
@@ -4,32 +4,54 @@ import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import "./global.css";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import ReduxProvider from "./providers/ReduxProvider";
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    variable: "--font-poppins",
-    weight: ["300", "400", "500", "600", "700", "800"],
-    display: "swap",
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["300", "400", "500", "600", "700", "800"],
+  display: "swap",
 });
 
 export const metadata = {
-    title: "Tetrahedron",
-    description: "Manufacturing Consulting Firm In India",
+  title: "Tetrahedron",
+  description: "Manufacturing Consulting Firm In India",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en" className={poppins.variable}>
-            <head>
-                <link rel="stylesheet" href="/assets/css/elitecons.css" />
-                <link rel="stylesheet" href="/assets/css/elitecons-responsive.css" />
-            </head>
-            <body className={poppins.className} style={{ fontFamily: 'var(--font-poppins), Poppins, sans-serif', lineHeight: '1.6' }}>
-                <ReduxProvider> 
-                {children}
-                </ReduxProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" className={poppins.variable}>
+      <head>
+        <link rel="stylesheet" href="/assets/css/elitecons.css" />
+        <link rel="stylesheet" href="/assets/css/elitecons-responsive.css" />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5JRTDX9G');
+          `}
+        </Script>
+      </head>
+      <body
+        className={poppins.className}
+        style={{
+          fontFamily: "var(--font-poppins), Poppins, sans-serif",
+          lineHeight: "1.6",
+        }}
+      >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JRTDX9G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
+    </html>
+  );
 }
