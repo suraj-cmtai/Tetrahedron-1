af749ec321f8769ba66840fbbaee40a931f63dc9
Vishal Sharma <vishal.sharma@completrix.com>
2025-09-03 11:14:59 +0530

    Integrate <Script> and <noScript> tag in thankyou page

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -3,6 +3,7 @@
 import Layout from "@/components/layout/Layout";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Script from "next/script";
 
 export default function Page() {
   const router = useRouter();
@@ -37,6 +38,27 @@ export default function Page() {
 
   return (
     <Layout>
+      {/* ✅ Google Tag Manager (Head) */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5JRTDX9G');
+        `}
+      </Script>
+
+      {/* ✅ Google Tag Manager (Body – noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5JRTDX9G"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
       <div style={pageWrapper}>
         {/* Background Shapes */}
         <div style={bgShapes}>
