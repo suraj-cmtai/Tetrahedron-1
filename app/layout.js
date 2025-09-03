a80f4a0de947775908e08820c8a0fd192a0b66d8
Vishal Sharma <vishal.sharma@completrix.com>
2025-09-03 12:52:17 +0530

    replate the script tag of app layout page

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5JRTDX9G');
+            })(window,document,'script','dataLayer','GTM-WBZD4FZN');
           `}
         </Script>
       </head>
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5JRTDX9G"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBZD4FZN"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
